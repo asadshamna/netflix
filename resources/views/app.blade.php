6e992f47390f9dd7f01c7eb516e6d94bfdcce218
Asad <asadshamna02@gmail.com>
2023-10-30 20:10:54 +0530

    Initial Setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<!-- Start designing the APP's Sign up or login page in this file -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="text-center mt-40">
+        <h1 class="text-3xl font-bold underline">
+            Netflix Project
+        </h1>
+        <ul class="list-disc">
+            <p>Project By:</p>
+            <li>Asad Shamna</li>
+            <li>Humayun Khan</li>
+            <li>Gulam Nabi Mundas</li>
+            <li>Fatima</li>
+        </ul>
+    </div>
+
+</body>
+
+</html>
